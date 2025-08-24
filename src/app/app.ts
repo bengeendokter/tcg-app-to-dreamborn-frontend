64b180ce7f959492619eed9d6b6abf3550243010
bengeendokter <benifit.arts@gmail.com>
2025-08-24 14:20:55 +0200

    Implement convert

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { backupDeck } from 'lorcana-tcg-to-dreamborn/src/backup-deck';
+import { DreambornDeck } from 'lorcana-tcg-to-dreamborn/src/model/dreamborn';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +13,27 @@ export class App {
   protected url: FormControl<string | null> = new FormControl('');
   protected result: FormControl<string | null> = new FormControl('');
 
-  protected convert(): void {
-    this.result.setValue(this.url.value);
+  protected async convert(): Promise<void> {
+    const backupUrl: string | null = this.url.value;
+
+    if (!backupUrl) {
+      alert('Please enter a valid URL');
+      return;
+    }
+
+    try {
+      const deck: DreambornDeck = await backupDeck(backupUrl);
+      const deckImportString: string = deck.cards.map(card => [card.count, card.fullName].join(' ')).join('\n');
+      this.result.setValue(deckImportString);
+    }
+    catch (error: unknown) {
+      if (error instanceof Error) {
+        alert(`Error: ${error.message}`);
+      }
+      else {
+        alert('An unknown error occurred');
+      }
+    }
   }
 
   protected async copyToClipboard(): Promise<void> {

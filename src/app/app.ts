b1d0f07ffd8b9c97cc067b09eea2a3c80daf621d
bengeendokter <benifit.arts@gmail.com>
2025-08-24 12:38:59 +0200

    Implement forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [ReactiveFormsModule],
 })
 export class App {
+  protected url: FormControl<string | null> = new FormControl('');
+  protected result: FormControl<string | null> = new FormControl('');
+
+  protected convert(): void {
+    this.result.setValue(this.url.value);
+  }
+
+  protected async copyToClipboard(): Promise<void> {
+    await navigator.clipboard.writeText(this.result.value ?? '');
+  }
 }
